9db41a21a39abdf83d9e476c3ae17e28a15670b3
CTortosaM <CTortosaM@users.noreply.github.com>
2018-04-17 11:09:48 +0200

    Logueo
    Sustituido el campo de "Usuario" por el de "Email", así como implementadas las tablas de sensores, mediciones y campos

--- server.js
+++ server.js
@@ -31,13 +31,15 @@ servidor.get("/", function(peticion, respuesta) {
   respuesta.sendFile(path.join(__dirname + "/paginas/index.html"));
 });
 
-servidor.post('/cambioContrasenya', cambiarContrasenya);
+servidor.put('/cambioContrasenya', cambiarContrasenya);
+
+servidor.put('/setUsuarioActivo', setUsuarioActivo);
 
 //servidor.get('/lobby', procesarUsuario);
 //BASE DATOS
 var sqlite3 = require('sqlite3');
-base_datos = new sqlite3.Database('base_datos.db',function(err){
-  if(err != null) {
+base_datos = new sqlite3.Database('base_datos.db', function(err) {
+  if (err != null) {
     respuesta.sendStatus(503);
   }
 });
@@ -48,7 +50,7 @@ var objUsuario = {};
 function procesar_login(peticion, respuesta) {
   function procesar_login2(err, row) {
     if (err != null) {
-      respuesta.send('Error de base de datos: ' + err);
+      respuesta.sendStatus(503);
     } else {
       if (row === undefined) {
         objUsuario = {
@@ -75,7 +77,7 @@ function procesar_login(peticion, respuesta) {
       } //else
     } //else
   } //procesar_login2
-  base_datos.get('SELECT * FROM usuarios WHERE nombre=?', [peticion.query.user], procesar_login2);
+  base_datos.get('SELECT * FROM usuarios WHERE email=?', [peticion.query.email], procesar_login2);
 
 } //procesarLogin
 
@@ -83,7 +85,7 @@ function procesar_login(peticion, respuesta) {
 // FUNCIÓN QUE SE EJECUTA AL "SUBMIT" LA NUEVA CONTRASEÑA
 function cambiarContrasenya(peticion, respuesta) {
 
-  base_datos.all('UPDATE usuarios SET contrasenya=? WHERE nombre="Carlos"', [peticion.query.contrasenya], function(err, row) {
+  base_datos.all('UPDATE usuarios SET contrasenya=? WHERE id=' + peticion.query.id, [peticion.query.contrasenya], function(err, row) {
     if (err != null) {
       respuesta.sendStatus(503);
     } else {
@@ -92,8 +94,19 @@ function cambiarContrasenya(peticion, respuesta) {
   });
 
 }
+//---------------------------------------------------------------------------------
+//FUNCIÓN PARA VOLVER ACTIVO UN USUARIO QUE YA SE HA LOGUEADO
+function setUsuarioActivo(peticion, respuesta) {
+  base_datos.all('UPDATE usuarios SET activo=1 WHERE id=' + peticion.query.id, function(err) {
+    if (err != null) {
+      respuesta.sendStatus(503);
+    } else {
+      respuesta.sendStatus(200);
+    } //else
+  });
+}
 
 
-servidor.listen(50971, function(){
+servidor.listen(50971, function() {
   console.log('En marcha');
 })
